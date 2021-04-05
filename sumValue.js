9d863d86ef82a60c4524f3e1c1ce7954988c2a34
sohnr <64889722+sohnr@users.noreply.github.com>
2021-04-05 14:36:36 +0900

    4월5일

---
+++ sumValue.js
@@ -0,0 +1,7 @@
+function sumValue(){
+		let a = document.getElementById("v1").value;
+		let b = document.getElementById("v2").value;
+		document.getElementById("v3").innerHTML = Number(a)+Number(b);
+	}/**
+ * 
+ */
